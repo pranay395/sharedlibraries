7ba53ca57d5724378e77e5eb16c29e18fb19c176
pranay395 <98809812+pranay395@users.noreply.github.com>
2022-02-21 12:40:15 +0530

    b

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,6 @@ def newMaven()
 }
 def newDeploy(qa)
 {
-  
   "deploy adapters: [tomcat9(credentialsId: '8be0084a-4288-40dc-9634-fc085c98e8fc', path: '', url: 'http://172.31.21.191:8080')], contextPath: 'test1', war: '**/*.war'"
 }
 def runSelenium(path)
@@ -17,6 +16,5 @@ def runSelenium(path)
 }
 def newDeploy(prod)
 {
-  
   "deploy adapters: [tomcat9(credentialsId: '8be0084a-4288-40dc-9634-fc085c98e8fc', path: '', url: 'http://172.31.24.74:8080')], contextPath: 'prod1', war: '**/*.war'"
 }
